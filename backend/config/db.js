b89312a4e7558bcbe00755d4ab9577e65358ab14
Druid45ra <radu_vanca@live.com>
2025-02-08 18:30:56 +0200

    update database connection settings and enhance error middleware response

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    mongoose.set("strictQuery", false); // Adaugă această linie pentru a evita avertismentul strictQuery
     await mongoose.connect("mongodb://localhost:27017/taskmanager", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    mongoose.set("strictQuery", false); // Adaugă această linie pentru a evita avertismentul strictQuery
     console.log("MongoDB Connected...");
   } catch (error) {
     console.error(`Error: ${error.message}`);
