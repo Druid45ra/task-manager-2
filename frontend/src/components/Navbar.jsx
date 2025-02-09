b4006c125a155b7d381a4031b69a519e9c274b00
Druid45ra <radu_vanca@live.com>
2025-02-09 16:41:19 +0200

    add react-router-dom for routing and implement task fetching in App component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,17 +1,14 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav>
-      <div className="nav-wrapper">
-        <Link to="/" className="brand-logo">Task Manager</Link>
-        <ul id="nav-mobile" className="right hide-on-med-and-down">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/tasks">Tasks</Link></li>
-          <li><Link to="/about">About</Link></li>
-        </ul>
-      </div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+      </ul>
     </nav>
   );
 };
